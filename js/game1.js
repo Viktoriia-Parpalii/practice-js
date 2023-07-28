b3fee8eaf20c0b42745019bbae3154fdf542c0e1
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-28 22:56:56 +0300

    fix game1

--- js/game1.js
+++ js/game1.js
@@ -19,3 +19,63 @@ const combination = [
   [3, 5, 7],
   [3, 6, 9],
 ];
+
+const historyX = [];
+const historyO = [];
+
+let player = "X";
+
+const content = document.querySelector(".content");
+
+content.addEventListener("click", onClick);
+
+createMarkup();
+
+function createMarkup() {
+  let markup = "";
+  for (i = 1; i <= 9; i += 1) {
+    markup += `<div class="item" data-id="${i}"></div>`;
+  }
+  content.innerHTML = markup;
+}
+
+function onClick(e) {
+  if (e.target.textContent || e.target === e.currentTarget) {
+    return;
+  }
+  const id = Number(e.target.dataset.id);
+  let isWinner = false;
+
+  if (player === "X") {
+    historyX.push(id);
+    isWinner = historyX.length >= 3 ? checkWinner(historyX) : false;
+  } else {
+    historyO.push(id);
+    isWinner = historyO.length >= 3 ? checkWinner(historyO) : false;
+  }
+
+  if (isWinner) {
+    const instance = basicLightbox.create(`
+      <div class="box">
+        <h1>Player ${player} is winner!</h1>
+      </div>
+    `);
+    instance.show();
+    resetGame();
+    return;
+  }
+
+  e.target.textContent = player;
+  player = player === "X" ? "O" : "X";
+}
+
+function checkWinner(history) {
+  return combination.some((item) => item.every((id) => history.includes(id)));
+}
+
+function resetGame() {
+  createMarkup();
+  player = "X";
+  historyX.splice(0);
+  historyO.splice(0);
+}
